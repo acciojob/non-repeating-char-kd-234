c132fafea0e74975f52fc68bc9a64d21e380c76e
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2023-09-09 23:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
 
+	let mp=new Map();
 	for(let i=0;i<str.length;i++){
-		if( str.indexOf(str.charAt(i))== str.lastIndexOf(str.chartAt(i))){
-
-			break;	
+		if(mp.get(str[i])){
+			mp.set(str[i],mp.get(str[i])+1);
+		}
+		else{
+			mp.set(str[i],1);
 		}
 	}
+	for(let i=0;i<str.length;i++){
+
+		if(mp.get(str[i])==1){
+
+			return str[i];
+		}
+		
+			
+	}
+		
+	return null;	
+	
 
 	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+
